6efb70ce4b9aaf9103d536edce7fee5e260bdb77
oscarporras02 <oscarrussi@outlook.com>
2021-07-02 17:19:37 -0500

    update entity context with new tables and using partial to protect previous changes in the model

---
+++ chess-memo/Models/AdditionalContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace chess_memo.Models
+{
+    public partial class chessmemoContext : DbContext
+    {
+        public virtual DbSet<Message> Messages { get; set; }
+    }
+}
---
+++ chess-memo/Models/Position.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace chess_memo.Models
+{
+    public partial class Position
+    {
+        public int Id { get; set; }
+        public string Board { get; set; }
+        public int TotalBlackPieces { get; set; }
+        public int TotalWhitePieces { get; set; }
+        public bool BlackLongCastling { get; set; }
+        public bool BlackShortCastling { get; set; }
+        public bool WhiteLongCastling { get; set; }
+        public bool WhiteShortCastling { get; set; }
+        public string LastMove { get; set; }
+        public int AvailableMoves { get; set; }
+    }
+}
