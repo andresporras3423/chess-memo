e11d30a6503548e2840bf6513f78c22901e1d23a
oscarporras02 <oscarrussi@outlook.com>
2021-07-04 12:04:08 -0500

    add endpoints to get difficulties and player config

--- chess-memo/Controllers/ConfigController.cs
+++ chess-memo/Controllers/ConfigController.cs
@@ -10,6 +10,27 @@ namespace chess_memo.Controllers
 {
     public class ConfigController : BaseController
     {
+
+        [HttpGet]
+        public Dictionary<string, string> Get()
+        {
+            using (var context = new chessmemoContext())
+            {
+                try
+                {
+                    Dictionary<string, string> body = read_body();
+                    Config playerConfig = context.Configs.Where(x => x.PlayerId == Int32.Parse(HttpContext.Session.GetString("id"))).FirstOrDefault();
+                    return new Dictionary<string, string> { { "DifficultyId", $"{playerConfig.DifficultyId}"},
+                    { "Questions", $"{playerConfig.Questions}" }};
+                    ;
+                }
+                catch (Exception ex)
+                {
+                    return new Dictionary<string, string> { { "error_message" , ex.Message } };
+                }
+            }
+        }
+
         [HttpPut]
         public string Put()
         {
---
+++ chess-memo/Controllers/DifficultyController.cs
@@ -0,0 +1,34 @@
+﻿using chess_memo.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace chess_memo.Controllers
+{
+    public class DifficultyController : BaseController
+    {
+        [HttpGet]
+        public List<Dictionary<string, string>> Get()
+        {
+            using (var context = new chessmemoContext())
+            {
+                try
+                {
+                    Dictionary<string, string> body = read_body();
+                    List<Dictionary<string, string>> difficulties = context.Difficulties.Select( d =>
+                    new Dictionary<string, string> { { "id", d.Id.ToString() },
+                    { "difficultyName", d.DifficultyName },
+                    { "minPieces", d.MinPieces.ToString() },
+                    { "maxPieces", d.MaxPieces.ToString() }}).ToList<Dictionary<string, string>>();
+                    return difficulties;
+                }
+                catch (Exception ex)
+                {
+                    return new List<Dictionary<string, string>> { new Dictionary<string, string> { { "error_message", ex.Message } }};
+                }
+            }
+        }
+    }
+}
